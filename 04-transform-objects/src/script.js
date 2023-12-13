553c02e9b22b4d93a502cbc881907b9b37c45749
KenLam <61927124+LamWK1029@users.noreply.github.com>
2023-12-13 15:57:42 +0800

    Add files via upload

---
+++ 04-transform-objects/src/script.js
@@ -0,0 +1,73 @@
+import * as THREE from "three";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// axes helper
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
+/**
+ * Objects
+ */
+
+// Group
+const group = new THREE.Group();
+scene.add(group);
+
+// mesh
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = 0.5;
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -0.5;
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+// add to group
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+
+// rotate group
+group.rotation.y = 0.5;
+
+// scale group
+group.scale.set(0.5, 0.5, 0.5);
+
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(1, 1, 5);
+scene.add(camera);
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
